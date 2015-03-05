761250c8bfc5bb2f145edccbac6292de87f1a2af
Rich Shea <richcshea@gmail.com>
2015-03-05 12:37:00 -0700

    add user model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150304221132) do
+ActiveRecord::Schema.define(version: 20150305192413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,17 @@ ActiveRecord::Schema.define(version: 20150304221132) do
   end
 
   add_index "jobs", ["city_id"], name: "index_jobs_on_city_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "nickname"
+    t.string   "email"
+    t.string   "provider"
+    t.string   "token"
+    t.string   "image_url"
+    t.string   "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   add_foreign_key "jobs", "cities"
 end
